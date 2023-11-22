2855ea6a2422aeeeeee37f340554513d52ae5f78
Abir Santra <santraabir8@gmail.com>
2023-11-22 11:16:29 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import Authpage from "./pages/auth-page";
 import Homepage from "./pages/home-page";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/auth",
+    element: <Authpage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <RouterProvider router={router} />
     </div>
   );
 }
