b2b39cb71c387ae4dd7ed3f78ebf03704872049c
Abir Santra <santraabir8@gmail.com>
2023-11-23 18:06:29 +0530

    add offer banner slider component

---
+++ src/components/slider.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+
+const Slider = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <div className="w-full max-w-[1440px]">
+        <div className="relative w-full select-none overflow-hidden rounded-xl">
+          <Swiper
+            modules={[Pagination, EffectFade, Autoplay]}
+            effect={"fade"}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+            pagination={{ dynamicBullets: true }}
+            slidesPerView={1}
+            loop={true}
+            className="w-full"
+          >
+            <SwiperSlide>
+              <div className="flex h-96 items-center justify-center bg-slate-200">
+                Slide 1
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex h-96 items-center justify-center bg-slate-200">
+                Slide 2
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex h-96 items-center justify-center bg-slate-200">
+                Slide 3
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex h-96 items-center justify-center bg-slate-200">
+                Slide 4
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Announcement from "../components/announcement";
+import Slider from "../components/slider";
 
 const Homepage = () => {
   return (
     <div>
       <Announcement text={"Sample Announcement!"} />
       <Navbar />
+      <Slider />
     </div>
   );
 };
