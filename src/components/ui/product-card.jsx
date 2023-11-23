3887c849f78b95ca0e65e0a6c2c266f8a01d8299
Abir Santra <santraabir8@gmail.com>
2023-11-23 18:36:10 +0530

    update product card colors

--- src/components/ui/product-card.jsx
+++ src/components/ui/product-card.jsx
@@ -4,36 +4,32 @@ import { MdOutlineAddShoppingCart } from "react-icons/md";
 
 const ProductCard = ({ imgUrl, mrp, dmrp, title, rating }) => {
   return (
-    <div className="productcard hover:border-primary group flex flex-col rounded-lg border-[1px] border-neutral-200 duration-300 ease-in-out">
+    <div className="group flex flex-col rounded-lg border-[1px] border-neutral-200 duration-300 ease-in-out hover:border-orange-600">
       {/* Product Image */}
-      <img className="product--image w-full" src={imgUrl} alt="title" />
-      <div className="divider group-hover:bg-primary h-[1px] w-full bg-neutral-200 duration-300 ease-in-out"></div>
+      <img className="w-full" src={imgUrl} alt="title" />
+      <div className="h-[1px] w-full bg-neutral-200 duration-300 ease-in-out group-hover:bg-orange-600"></div>
       {/* Product Description */}
-      <div className="description flex flex-col gap-2 p-4">
+      <div className="flex flex-col gap-2 p-4">
         {/* Price */}
-        <div className="pricing flex items-center gap-4">
-          <p className="discountedprice text-primary text-[24px] font-bold">
-            ${dmrp}
-          </p>
-          <p className="originalprice font-semibold text-neutral-400 line-through">
-            ${mrp}
-          </p>
+        <div className="flex items-center gap-4">
+          <p className="text-[24px] font-bold text-orange-600">${dmrp}</p>
+          <p className="font-semibold text-neutral-400 line-through">${mrp}</p>
         </div>
         {/* Title */}
-        <p className="title text-lg font-semibold">{title}</p>
+        <p className="text-lg font-semibold">{title}</p>
         {/* Rating and Add to Cart */}
-        <div className="rating--addtocart flex w-full items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           {/* Rating */}
-          <div className="rating--stars flex gap-1">
-            <FaStar className="text-primary" />
-            <FaStar className="text-primary" />
-            <FaStar className="text-primary" />
-            <FaStar className="text-primary" />
+          <div className="flex gap-1">
+            <FaStar className="text-orange-600" />
+            <FaStar className="text-orange-600" />
+            <FaStar className="text-orange-600" />
+            <FaStar className="text-orange-600" />
             <FaStar className="text-neutral-400" />
           </div>
           {/* Add to Cart */}
           <button>
-            <span className="bg-primary flex h-12 w-12 items-center justify-center rounded-full text-white duration-300 ease-in-out hover:scale-105">
+            <span className="flex h-12 w-12 items-center justify-center rounded-full bg-orange-600 text-white duration-300 ease-in-out hover:scale-105">
               <MdOutlineAddShoppingCart size={24} />
             </span>
           </button>
