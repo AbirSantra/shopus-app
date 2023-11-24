6ed7dc5fe0828068f38fbdd15b9a82d4d11fff07
Abir Santra <santraabir8@gmail.com>
2023-11-24 18:38:10 +0530

    change font weight

--- src/components/announcement.jsx
+++ src/components/announcement.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Announcement = ({ text }) => {
   return (
-    <div className="fluid-px flex w-full items-center justify-center bg-orange-600 py-1 text-sm font-semibold text-white">
+    <div className="fluid-px flex w-full items-center justify-center bg-orange-600 py-1 text-sm font-normal text-white">
       <p className="animate-pulse">{text}</p>
     </div>
   );
